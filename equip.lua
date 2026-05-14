c90f2890fca79f023ccd4390a1e7c3cab853cb0a
spicyflamables <282393534+spicyflamables@users.noreply.github.com>
2026-05-14 20:12:42 +0100

    Update equip.lua

--- equip.lua
+++ equip.lua
@@ -1,32 +1,17 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/spicyflamables/funnymonke/refs/heads/main/items.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/spicyflamables/funnymonke/main/items.lua"))()
 
-if not getgenv().inventorystuff["Wallet"] then
-	warn("wallet not found in inventory")
-	return
-end
+local itemname = getgenv().inventorystuff["Wallet"]
 
-local actor = game:GetService("Players").LocalPlayer.PlayerScripts.Actor
+local player = game:GetService("Players").LocalPlayer
+local actor = player.PlayerScripts.Actor
 
-run_on_actor(actor, [[
-	local players = game:GetService("Players")
-	local player = players.LocalPlayer
+run_on_actor(actor, string.format([[
+	local player = game:GetService("Players").LocalPlayer
 
-	local item
-
-	for _, v in ipairs(player.Backpack:GetChildren()) do
-		if v.Name:lower():find("wallet") then
-			item = v
-			break
-		end
-	end
+	local item = player.Backpack:FindFirstChild("%s")
 
 	if not item and player.Character then
-		for _, v in ipairs(player.Character:GetChildren()) do
-			if v.Name:lower():find("wallet") then
-				item = v
-				break
-			end
-		end
+		item = player.Character:FindFirstChild("%s")
 	end
 
 	if not item then
@@ -43,4 +28,4 @@ run_on_actor(actor, [[
 	else
 		warn("tool not found")
 	end
-]])
+]], itemname, itemname))
