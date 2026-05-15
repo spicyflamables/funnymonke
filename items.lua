2bb3c66bfe62bdcd55e5e3cfa522b0bfec55c7a4
spicyflamables <282393534+spicyflamables@users.noreply.github.com>
2026-05-15 17:45:45 +0100

    Update items.lua

--- items.lua
+++ items.lua
@@ -3,16 +3,23 @@ local items = require(game:GetService("ReplicatedStorage").Databases.Items)
 
 getgenv().inventorystuff = {}
 
+local color = Color3.fromRGB(55, 103, 159)
+
 for _, v in ipairs(inventory:GetDescendants()) do
 	if v:IsA("ImageButton") then
 		local icon = v:FindFirstChild("IconLabel")
+		local slot = v:FindFirstChild("SlotLabel")
 
 		if icon and icon:IsA("ImageLabel") then
 			local id = icon.Image
+			local slottext = slot and slot:IsA("TextLabel") and slot.Text or "what"
 
 			for _, data in pairs(items) do
 				if typeof(data) == "table" and data.HotbarThumb == id then
-					getgenv().inventorystuff[data.Name] = v.Name
+					getgenv().inventorystuff[data.Name] = {
+						slot = slottext,
+						equipped = v.BackgroundColor3 == color
+					}
 				end
 			end
 		end
